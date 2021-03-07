d0f90eabf1f7e33c143cd7af46ea9eb354562406
Ubuntu <ubuntu@ip-172-31-41-226.af-south-1.compute.internal>
2021-03-07 23:43:15 +0000

    asd

--- prepare.sh
+++ prepare.sh
@@ -2,4 +2,4 @@ git submodule update --init --recursive
 ./tools/install_deps.sh
 src/experiments/setup.py --install-deps --all
 src/experiments/setup.py --setup --all
-
+sudo sysctl -w net.ipv4.ip_forward=1
