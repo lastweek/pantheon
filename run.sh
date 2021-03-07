a0641eb6b033ecc17da7384c72d348e5ca96d0d1
Yizhou Shan <syzwhat@gmail.com>
2021-03-07 21:16:02 +0000

    asd

--- run.sh
+++ run.sh
@@ -1,5 +1,18 @@
-PEM_FILE=/home/ubuntu/aws-pem/cse222a-oregon.pem
+set -x
+set -e
+
+PEM_FILE=/home/ubuntu/aws-pem/cse222a-frankfurt.pem
+DATA_DIR=/home/ubuntu/pantheon/eval/virginia-frankfurt-1f
+
+# 52.39.71.112 oregon
+# 3.124.185.209 frankfurt
 
 src/experiments/test.py remote --all --run-times 3 -t 30 \
-    ubuntu@52.39.71.112:/home/ubuntu/pantheon  \
-    --pem $PEM_FILE 
+    ubuntu@3.124.185.209:/home/ubuntu/pantheon  \
+    --pem $PEM_FILE  \
+    --data-dir $DATA_DIR
+
+src/analysis/analyze.py --data-dir $DATA_DIR
+
+git add $DATA_DIR
+git commit -m "Add $DATA_DIR"
