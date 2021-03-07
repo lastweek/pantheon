cd0906347fa7c70e0967cf97ea74e99cadeaddd4
Yizhou Shan <syzwhat@gmail.com>
2021-03-07 22:46:23 +0000

    asd

---
+++ prepare.sh
@@ -0,0 +1,5 @@
+git submodule update --init --recursive
+./tools/install_deps.sh
+src/experiments/setup.py --install-deps --all
+src/experiments/setup.py --setup --all
+
--- run.sh
+++ run.sh
@@ -1,14 +1,19 @@
 set -x
 set -e
 
-PEM_FILE=/home/ubuntu/aws-pem/cse222a-frankfurt.pem
-DATA_DIR=/home/ubuntu/pantheon/eval/virginia-frankfurt-1f
+#PEM_FILE=/home/ubuntu/aws-pem/cse222a-frankfurt.pem
+#DATA_DIR=/home/ubuntu/pantheon/eval/virginia-frankfurt-1f
+
+PEM_FILE=/home/ubuntu/aws-pem/cse222a-virginia.pem
+IP=44.192.83.123
+DATA_DIR=/home/ubuntu/pantheon/eval/frankfurt-virginia-1f
 
 # 52.39.71.112 oregon
+# 44.192.83.123 virginia
 # 3.124.185.209 frankfurt
 
 src/experiments/test.py remote --all --run-times 3 -t 30 \
-    ubuntu@3.124.185.209:/home/ubuntu/pantheon  \
+    ubuntu@$IP:/home/ubuntu/pantheon  \
     --pem $PEM_FILE  \
     --data-dir $DATA_DIR
 
@@ -16,3 +21,4 @@ src/analysis/analyze.py --data-dir $DATA_DIR
 
 git add $DATA_DIR
 git commit -m "Add $DATA_DIR"
+git push
